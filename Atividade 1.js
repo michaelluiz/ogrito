350b48846e3607d821fed0158c8840cc638f824a
Michael Luiz <michael.luiz.2013@gmail.com>
2021-03-23 20:52:50 -0300

    Criando Projeto Console

---
+++ Atividade 1.js
@@ -0,0 +1,4 @@
+const readline = require('readline')
+
+console.log ("inserir cartão")
+readline ()
---
+++ O grito aula - 01/O grito aula - 01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace O_grito_aula___01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira seu cartão");
+            Console.ReadLine();
+            Console.WriteLine("Digite sua senha de 4 digitos");
+            Console.ReadLine();
+            Console.WriteLine("Selecione o serviço");
+            Console.ReadLine();
+            Console.WriteLine("Digite o valor");
+            Console.ReadLine();
+            Console.WriteLine("Digite sua senha de 4 digitos");
+            Console.ReadLine();
+            Console.WriteLine("aguarde e a contagem das cedulas, e confira o valor");
+            Console.WriteLine("Deseja outro serviço?");
+            string continuar;
+            continuar = Console.ReadLine();
+            Console.WriteLine(continuar);
+
+        }
+    }
+}
