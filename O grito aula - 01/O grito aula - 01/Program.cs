ae6a5ea493a37279f5803fdbba19b7ff96c63bed
Michael Luiz <michael.luiz.2013@gmail.com>
2021-04-13 21:00:14 -0300

    Condicionais aula dia 13/04/2021

--- O grito aula - 01/O grito aula - 01/Program.cs
+++ O grito aula - 01/O grito aula - 01/Program.cs
@@ -51,22 +51,69 @@ namespace O_grito_aula___01
             //IMC = pesoEmKg / (alturaEmMetros * alturaEmMetros);
             //Console.WriteLine(IMC);
 
-            double valorDoProduto;
-            double lucro;
-            double venda;
-            Console.WriteLine("valor de Compra");
-            valorDoProduto = double.Parse(Console.ReadLine());
-            lucro = valorDoProduto * 0.43;
-            Console.WriteLine("lucro");
-            Console.WriteLine(lucro);
-            venda = valorDoProduto + lucro;
-            Console.WriteLine("Valor de Venda");
-            Console.WriteLine(venda);
+            //double valorDoProduto;
+            //double lucro;
+            //double venda;
+            //Console.WriteLine("valor de Compra");
+            //valorDoProduto = double.Parse(Console.ReadLine());
+            //lucro = valorDoProduto * 0.43;
+            //Console.WriteLine("lucro");
+            //Console.WriteLine(lucro);
+            //venda = valorDoProduto + lucro;
+            //Console.WriteLine("Valor de Venda");
+            //Console.WriteLine(venda);
 
 
-            
+            //bool valor1;
+            //bool valor2;
+            //valor1 = true;
+            //valor2 = false;
+            //if (valor2)
+            //{
+            //    Console.WriteLine("Valor 2 estava verdadeiro");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Valor 1 estava verdadeiro");
+            //}
+
+            //bool notVerdadeiro = !true; //falso
+            //bool notFalso = !false; //verdadeiro
+            //bool ou1 = true || true; // verdadeiro
+            //bool ou2 = true || false; // verdadeiro
+            //bool ou3 = false || true; // verdadeiro
+            //bool ou4 = false || false; // falso
+            //bool e1 = true && true; // verdadeiro
+            //bool e2 = true && false; // falso
+            //bool e3 = false && true; // falso
+            //bool e4 = false && false; // falso
+            //Console.WriteLine(notVerdadeiro);
+            //Console.WriteLine(notFalso);
+            //Console.WriteLine(ou1);
+            //Console.WriteLine(ou2);
+            //Console.WriteLine(ou3);
+            //Console.WriteLine(ou4);
+            //Console.WriteLine(e1);
+            //Console.WriteLine(e2);
+            //Console.WriteLine(e3);
+            //Console.WriteLine(e4);
 
 
-         }
+            // Se o meu usuário informar 1, eu mostro na tela "Válido",
+            // caso contrario mostrar "Invalido"
+            //bool n1;
+
+            //Console.WriteLine("informe 1 ou 0");
+            //n1 = bool.Parse (Console.ReadLine());
+
+            //if (n1)
+            //{
+            //    Console.WriteLine("Válido");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("inválido");
+            }
+        }
     }
 }
